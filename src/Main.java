0446d454c1db3c928baabd1620d83318b167090c
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-05 13:29:13 +0300

    Excellent number finder working.

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp =new Scanner(System.in);
-        System.out.print("How much number you want to enter: ");
+        System.out.print("Enter the number: ");
         int n = inp.nextInt();
-        int min=0, max=0,number;
-        for(int i=1;i<=n;i++)
+        int divisionTotal=0;
+
+        for (int i=1;i<n; i++)
         {
-            System.out.print("Enter the "+i+". number: ");
-            number= inp.nextInt();
-           if(i==1)
-           {
-               max=number;
-               min=number;
-           }
-           else if (number<=min)
-           {
-               min=number;
-           }
-           else
-           {
-               max=number;
-           }
+            if(n%i==0)
+            {
+                divisionTotal+=i;
+            }
         }
-        System.out.println("Biggest number is: "+max);
-        System.out.println("Smallest number is: "+min);
+        if(divisionTotal==n)
+            System.out.println(n+" is the Excellent number.");
+        else System.out.println(n+" isn't Excellent number.");
     }
 }
