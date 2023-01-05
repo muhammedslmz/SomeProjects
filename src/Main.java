ea5e6f87e2778670098e049718c3ad2041ab1ebf
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-05 14:22:53 +0300

    Stars Pyramid.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inp =new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = inp.nextInt();
-        int divisionTotal=0;
+       for(int i =0; i<10;i++)
+       {
+           for(int j=0;j<i;j++ )
+           {
+               System.out.print(" ");
+           }
 
-        for (int i=1;i<n; i++)
-        {
-            if(n%i==0)
-            {
-                divisionTotal+=i;
-            }
-
-        }
-        if(divisionTotal==n)
-            System.out.println(n+" is the Excellent number.");
-        else System.out.println(n+" isn't Excellent number.");
+           for (int k=(i*2);k<19;k++ )
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
     }
 }
